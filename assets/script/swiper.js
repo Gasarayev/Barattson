51ecbbc8dedb94750365230b8ba6ef51b3197941
Gasarayev <gasarayev.emil@gmail.com>
2024-07-30 12:11:31 +0400

    slider section

---
+++ assets/script/swiper.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 10,
+//   autoplay: {
+    // delay: 2500,
+    // disableOnInteraction: false,
+//   },
+
+//   breakpoints: {
+//     1400: {
+//       slidesPerView: 4,
+//     },
+//     1200: {
+//       slidesPerView: 2,
+//     },
+//     768: {
+//       slidesPerView: 1,
+//     },
+//     0: {
+//       slidesPerView: 1,
+//     },
+//   },
+});
