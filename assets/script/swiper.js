91c682381010bb09b5ec943fc25beb61f5b59dd9
Gasarayev <gasarayev.emil@gmail.com>
2024-08-05 10:29:53 +0400

    footer

--- assets/script/swiper.js
+++ assets/script/swiper.js
@@ -8,6 +8,10 @@ function createSwiper(selector, slidesPerView) {
       delay: 2500,
       disableOnInteraction: false,
     },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
     breakpoints: {
       991: {
         slidesPerView: slidesPerView,
@@ -22,11 +26,40 @@ function createSwiper(selector, slidesPerView) {
   });
 }
 
+function createSwiperVac(selector, slidesPerView) {
+  return new Swiper(selector, {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: slidesPerView,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      991: {
+        slidesPerView: slidesPerView,
+      },
+      0: {
+        slidesPerView: slidesPerView -1,
+      },
+    },
+  });
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('.feedback')) {
     createSwiper('.feedback', 3);
   }
   if (document.querySelector('.slider')) {
     createSwiper('.slider', 4);
+  }
+  if (document.querySelector('.swiper-container_vacancy')) {
+    createSwiperVac('.swiper-container_vacancy', 2);
   }
 });
