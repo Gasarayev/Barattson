70bb0d190edd7876734dd6d86cfaa16c80e698f6
Gasarayev <gasarayev.emil@gmail.com>
2024-08-07 10:36:31 +0400

    about part

--- assets/script/aboutPage_category.js
+++ assets/script/aboutPage_category.js
@@ -1,16 +1,43 @@
 // const button = document.getElementsByClassName("active");
 
-
 // Array.from(button).forEach(btn => {
 //     addEventListener("click", (btn)=>{
 //         btn.classList.add("btn_active");
 //     })
-// }); 
+// });
 
-const buttons = document.getElementsByClassName("active");
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.getElementsByClassName("active");
+    const sections = document.querySelectorAll(".allPartNone");
 
-Array.from(buttons).forEach(button => {
-    button.addEventListener("click", () => {
+    console.log(Array.from(sections)[0])
+    
+    if (buttons.length > 0 && sections.length > 0) {
+      Array.from(buttons)[0].classList.add("btn_active");
+      Array.from(sections)[0].classList.remove("allPartNone");
+      Array.from(sections)[0].classList.add("allPart");
+
+    }
+  
+
+    Array.from(buttons).forEach((button, index) => {
+      button.addEventListener("click", () => {
+        
+        Array.from(buttons).forEach((btn) => {
+          btn.classList.add("about_btn");
+          btn.classList.remove("btn_active");
+        });
+  
+        Array.from(sections).forEach((part) => {
+          part.classList.add("allPartNone");
+        });
+  
         button.classList.add("btn_active");
+  
+        if (sections[index]) {
+          sections[index].classList.remove("allPartNone");
+        }
+      });
     });
-});
+  });
+  
